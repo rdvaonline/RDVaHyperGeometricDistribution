e0dcdc1f722ef446220d55fb8fb533960b437798
Rob VanHoorne <rdvaonline@Robs-MacBook-Pro-2.local>
2019-01-07 22:47:57 -0600

    Added tests

---
+++ RDVaHyperGeometricDistribution/HyperGeometricDistribution.swift
@@ -0,0 +1,10 @@
+//
+//  HyperGeometricDistribution.swift
+//  RDVaHyperGeometricDistribution
+//
+//  Created by Rob VanHoorne on 1/7/19.
+//  Copyright © 2019 Rob VanHoorne. All rights reserved.
+//
+
+import Foundation
+
